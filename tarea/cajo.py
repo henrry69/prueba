60339d09c03733388fef009468608603bf128196
henrry69 <cajorafaelh@gmail.com>
2019-11-07 22:30:36 -0600

    prueba

---
+++ tarea/cajo.py
@@ -0,0 +1,422 @@
+#ejercicio1
+edad=17
+nombre="Henrry Cajo Rafael"
+dni=75020374
+print("1.-" ,nombre, "tiene la edad de",edad ,"y su numero de dni es",dni)
+
+#ejercicio2
+codigo="190536e"
+alumno="Henrry Cajo Rafael"
+escuela="ing electronica"
+print("2.- El alumno",alumno,"tine el codigo",codigo, " y es de la escuela" ,escuela)
+
+#ejercicio3
+monto=270
+cursos=6
+alumno="Henrry Cajo Rafael"
+print("3.- El alumno",alumno,"pago el monto de",monto, "para matricularse en ",cursos,"cursos")
+
+#ejercicio4
+peso=70
+estatura=1.75
+paciente="Henrry Cajo Rafael"
+print("4.- El paciente",paciente,"pesa",peso,"y tiene una estatura de",estatura)
+
+#ejercicio5
+sexo="masculino"
+paciente="Henrry Cajo Rafael"
+edad=17
+print("5.- El paciente",paciente,"de sexo",sexo,"y tine la edad de",edad)
+
+#ejercicio6
+total=2.50
+consumidor="Henrry Cajo Rafael"
+producto="leche"
+print("6.- El conusmidor",consumidor,"compro un tarro de",producto," que le costo",total)
+
+#ejercicio7
+direccion="paracas 228"
+estudiante="Henrry Cajo Rafael"
+carrera="ing electronica"
+print("7.- El estudiante",estudiante,"de la carrera",carrera,"vive en la calle",direccion)
+
+#ejercicio8
+padre="Henrry Cajo Carlos"
+dni=75040394
+alumno="Henrry Cajo Rafael"
+print("8.- El padre ",padre,"del alumno",alumno,"tine el dni",dni)
+
+#ejercico9
+cliente="Andres"
+tarjeta=5564356280288872
+producto="polo"
+print("9.- El cliente",cliente,"pago el ",producto,"con la tarjeta de numero",tarjeta)
+
+#ejercicio10
+tienda="topitop"
+vendedor="Carlos"
+descuento=17.29
+print("10.- En la tienda",tienda,"el vendedor ",vendedor,"aplico un descuento de",descuento)
+
+#ejercicio11
+hora="10:29"
+pago=166
+cajero="donde se vende"
+print("11.- En la tinda",cajero,"se efectuo un pago de",pago,"soles a las horas de la",hora,"mañana")
+
+#ejercicio12
+material="fierro"
+total=29
+descuento=10
+print("12.- En la compra de un", material ,"se aplico un descuento de",descuento,"soles al total de ", total,"soles")
+
+#ejercicio13
+libro="venero"
+numero_de_tema=6
+tomo=3
+print("13.- El libro de",libro,"contiene ",numero_de_tema,"temas y ",tomo,"tomos")
+
+#ejercicio14
+fecha_de_pago="16/10/2019"
+recibo="luz"
+total_a_pagar=166
+print("14.- la fecha de pago es el",fecha_de_pago,"del recibo de",recibo,"y el total a pagar es de",total_a_pagar)
+
+#ejercico15
+dato= "Fecha de Corte"
+informacion="recibo de luz"
+valor= "22/11/2017"
+print("15.- La",dato,"del  ",informacion,"es el dia",valor)
+
+#ejercicio16
+dato= "Ciclo"
+informacion="matricula"
+valor= 27
+print("16.- En el pago de la ",informacion,"del ",dato,"fue de",valor)
+
+#ejercicio17
+dato= "Suministro"
+valor= 26057204
+informacion= "Recibo de luz"
+print("17.- En el",informacion,"su",dato, " es el numero",valor)
+
+#ejercicio18
+dato= "Contenido"
+valor= "180 mL"
+informacion= " LISTERINE"
+print("18.- El ",dato ,"del pomo de",informacion, "es el de",valor)
+
+#ejercicio19
+dato= "Lote"
+valor= 959
+informacion= " LISTERINE"
+print("19.-Su  ",dato,"de la marca",informacion,"es la de ",valor)
+
+#ejercicio20
+dato= "Exportador"
+valor= "Jhonson & Jhonson"
+informacion= " LISTERINE"
+print("20.-El ",dato,"de la marca ",informacion, "es",valor)
+
+#ejercicio21
+dato= "RUC"
+valor= 202040001007
+informacion= " LISTERINE"
+print("21.- El  ",dato,"de la marca",informacion,"es ",valor)
+
+
+#ejercicio22
+dato= "Telefono"
+valor= 800555333
+informacion= " LISTERINE"
+print("22.- El", dato ,"de la marca ",informacion,"es el ",valor)
+
+#ejercico23
+dato= "Cargo Fijo"
+valor= 3.14
+informacion= "Recibo de luz"
+print("23.- El ",dato,"del ",informacion,"es de",valor)
+
+#ejercico24
+dato= "Interes y Mora"
+valor= 0.14
+informacion= "Recibo de luz"
+print("24.- El ",dato,"del ",informacion,"es de",valor)
+
+#ejercico25
+dato= "Sub Total"
+valor= 174.73
+informacion= "Recibo de luz"
+print("25.- El",dato,"del ",informacion,"es de ",valor)
+
+#ejercico26
+dato= "I.G.V."
+valor= 26.05
+informacion= "Recibo de luz"
+print("26.- El",dato,"del ",informacion,"es de ",valor)
+
+#ejercicio27
+dato= "Redondeo"
+valor= "-0.05"
+informacion= "Recibo de luz"
+print("27.- El",dato,"del ",informacion,"es de ",valor)
+
+#ejercicio28
+dato= "Monto Recibido"
+valor= 172.9
+informacion= "Recibo de luz"
+print("28.- El",dato,"del ",informacion,"es de ",valor)
+
+#ejercicio29
+dato= "Saldo Anterior"
+valor= 181.7
+informacion= "Recibo de luz"
+print("29.- El",dato,"del ",informacion,"es de ",valor)
+
+#ejercicio30
+dato= "Total a Pagar"
+valor= 354.6
+informacion= "Recibo de luz"
+print("30.- El",dato,"del ",informacion,"es de ",valor)
+
+#ejercicio31
+dato= "Fecha de Emision"
+valor= "31/10/2017"
+informacion= "Recibo de luz"
+print("31.- El",dato,"del ",informacion,"es de ",valor)
+
+#ejercicio32
+dato= "Fecha de Vencimiento"
+valor= "21/11/2017"
+informacion= "Recibo de luz"
+print("32.- El",dato,"del ",informacion,"es de ",valor)
+
+#ejercicio33
+
+dato= "Contenido"
+valor= "3000 mL"
+informacion= "Etiqueta Gaseosa PEPSI"
+print("33.- El",dato,"del ",informacion,"es de ",valor)
+
+#ejercicio34
+dato= "Carbohidratos"
+valor= "15g"
+informacion= "Etiqueta Gaseosa PEPSI"
+print("34.- El",dato,"del ",informacion,"es de ",valor)
+
+#ejercicio34
+dato= "Proteinas"
+valor= 0
+informacion= "Etiqueta Gaseosa PEPSI"
+print("34.- El",dato,"del ",informacion,"es de ",valor)
+
+#ejercico35
+dato= "Codigo de Barras"
+informacion= "Etiqueta Gaseosa PEPSI"
+print("35.- El",dato,"de la " ,informacion)
+
+
+#ejercico36
+dato= "Sodio"
+valor= "5mg"
+informacion= "Etiqueta Gaseosa PEPSI"
+print("36.- El",dato,"del ",informacion,"es de ",valor)
+
+#ejercico37
+dato= "Facturacion"
+valor= "oct-17"
+informacion= "Recibo de luz"
+print("37.- El",dato,"del ",informacion,"es de ",valor)
+
+#ejercicio38
+dato= "Direccion"
+valor= "Conquista 975"
+informacion= "Recibo de luz"
+print("38.- El",dato,"del ",informacion,"es de ",valor)
+
+#ejercicio39
+dato= "N°de Recibo"
+valor= "251-35052830"
+informacion= "Recibo de luz"
+print("39.- El",dato,"del ",informacion,"es de ",valor)
+
+#ejercicio40
+dato= "Codigo"
+valor= 26056204
+informacion= "Recibo de luz"
+print("40.- El",dato,"del ",informacion,"es de ",valor)
+
+#ejercicio41
+dato= "Actividad"
+valor= "Consumo Electrico"
+informacion= "Recibo de luz"
+print("41.- El",dato,"del ",informacion,"es de ",valor)
+
+#ejercicio42
+dato= "Medidor"
+valor= "00000000008206906-Electron"
+informacion= "Recibo de luz"
+print("42.- El",dato,"del ",informacion,"es de ",valor)
+
+#ejercicio43
+dato= "Lectura Actual"
+valor= 402020000
+informacion= "Recibo de luz"
+print("43.- El",dato,"del ",informacion,"es de ",valor)
+
+#ejercicio44
+dato= "Consumo"
+valor= "246.00 KW/h"
+informacion= "Recibo de luz"
+print("44.- El",dato,"del ",informacion,"es de ",valor)
+
+#ejercicio45
+dato= "Factor"
+valor= 1
+informacion= "Recibo de luz"
+print("45.- El",dato,"del ",informacion,"es de ",valor)
+
+#ejercicio46
+dato= "Hilos"
+valor= 2
+informacion= "Recibo de luz"
+print("46.- El",dato,"del ",informacion,"es de ",valor)
+
+#ejercicio47
+dato= "Fecha"
+valor= "30/09/17"
+informacion= "Recibo de luz"
+print("47.- El",dato,"del ",informacion,"es de ",valor)
+
+#ejercicio48
+dato= "Tarifa"
+valor= "BT5B-Residencial"
+informacion= "Recibo de luz"
+print("48.- El",dato,"del ",informacion,"es de ",valor)
+
+#ejercicio49
+dato= "Faseta de Desgastes"
+valor= "Erosion"
+informacion= "Historia Clinica"
+print("49.- El",dato,"del ",informacion,"es de ",valor)
+
+#ejercicio50
+dato= "Periodonto"
+valor= "Papilar"
+informacion= "Historia Clinica"
+print("50.- El",dato,"del ",informacion,"es de ",valor)
+
+#ejercicio51
+dato= "Sangrado"
+valor= "Espontaneo"
+informacion= "Historia Clinica"
+print("51.- El",dato,"del ",informacion,"es de ",valor)
+
+#ejercicio52
+dato= "Movilidad Dentaria"
+valor= "Si"
+informacion= "Historia Clinica"
+print("52.- El",dato,"del ",informacion,"es de ",valor)
+
+#ejercicio53
+dato= "Edad"
+valor= "19"
+informacion= "Historia Clinica"
+print("53.- El",dato,"del ",informacion,"es de ",valor)
+
+#ejercicio54
+dato= "Sexo"
+valor= "Masculino"
+informacion= "Historia Clinica"
+print("54.- El",dato,"del ",informacion,"es de ",valor)
+
+#ejercicio55
+dato= "Firma de Esmatologo"
+informacion= "Historia Clinica"
+print("55.- la",informacion,"tiene la",dato)
+
+#ejercicio56
+dato= "Logo"
+informacion= "Envase Alcohol"
+print("56.- El ",dato ,"del " ,informacion)
+
+#ejercicio57
+dato= "Contenido Neto"
+valor= "120mL"
+informacion= "Envase Alcohol"
+print("57.- El",dato,"del ",informacion,"es de ",valor)
+
+#ejercicio58
+dato= "RUC"
+valor= 20501543277
+informacion= "Envase Alcohol"
+print("58.- El",dato,"del ",informacion,"es de ",valor)
+
+#ejercicio59
+dato= "Telefono"
+valor= 5670517
+informacion= "Envase Alcohol"
+print("59.- El",dato,"del ",informacion,"es de ",valor)
+
+#ejercicio60
+dato= "Contenido Neto"
+valor= "Alcohol Etilico"
+informacion= "Envase Alcohol"
+print("60.- El",dato,"del ",informacion,"es de ",valor)
+
+#ejercicio61
+dato= "Lote"
+valor= "R1111178"
+informacion= "Envase Alcohol"
+print("61.- El",dato,"del ",informacion,"es de ",valor)
+
+#ejercicio62
+dato= "Vence"
+valor= "Nov-23"
+informacion= "Envase Alcohol"
+print("62.- El",dato,"del ",informacion,"es de ",valor)
+
+#ejercicio63
+dato= "Laboratorio"
+valor= "ALKOFARMA E.I.R.L."
+informacion= "Envase Alcohol"
+print("63.- El",dato,"del ",informacion,"es de ",valor)
+
+#ejercicio64
+dato= "Oficio"
+valor= "1205-2018-DIGEMID"
+informacion= "Envase Alcohol"
+print("64.- El",dato,"del ",informacion,"es de ",valor)
+
+#ejercicio65
+dato= "Logo"
+informacion= "Etiqueta Gaseosa PEPSI"
+print("65.- El",dato,"del ",informacion,"es de ",valor)
+
+#ejercicio66
+dato= "Grasas Saturadas"
+valor= 0
+informacion= "Etiqueta Gaseosa PEPSI"
+print("66.- El",dato,"del ",informacion,"es de ",valor)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
